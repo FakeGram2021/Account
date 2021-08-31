0fc50132e78cc73cb249884827911c07546645a7
mdjokic <41081275+mdjokic@users.noreply.github.com>
2021-08-31 19:03:00 +0200

    User blocking and muting (#4)

--- src/main/java/fakegram/adapter/http/controller/v1/MuteController.java
+++ src/main/java/fakegram/adapter/http/controller/v1/MuteController.java
@@ -1,4 +1,52 @@
 package fakegram.adapter.http.controller.v1;
 
+import fakegram.domain.model.account.User;
+import fakegram.domain.service.MuteService;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.*;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authentication;
+
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.UUID;
+
+import static io.micronaut.security.rules.SecurityRule.IS_AUTHENTICATED;
+
+@Controller("/api/v1/mute")
+@Secured(IS_AUTHENTICATED)
 public class MuteController {
+
+    private final MuteService muteService;
+
+    @Inject
+    public MuteController(final MuteService muteService) {
+        this.muteService = muteService;
+    }
+
+    @Get("")
+    @Secured({"ROLE_USER"})
+    public HttpResponse<Collection<User>> getMutedUsers(Authentication authentication) {
+        UUID accountID = UUID.fromString(authentication.getName());
+        Collection<User> mutedUsers = muteService.getMutedUsers(accountID);
+        return HttpResponse.ok(mutedUsers);
+    }
+
+    @Post("/{muteUserId}")
+    @Secured({"ROLE_USER"})
+    public HttpResponse<?> muteUser(@PathVariable UUID muteUserId, Authentication authentication) {
+        UUID accountID = UUID.fromString(authentication.getName());
+        muteService.muteUser(accountID, muteUserId);
+        return HttpResponse.ok();
+    }
+
+    @Delete("/{muteUserId}")
+    @Secured({"ROLE_USER"})
+    public HttpResponse<?> unmuteUser(@PathVariable UUID muteUserId, Authentication authentication) {
+        UUID accountID = UUID.fromString(authentication.getName());
+        muteService.unmuteUser(accountID, muteUserId);
+        return HttpResponse.ok();
+    }
+
+
 }
---
+++ src/main/java/fakegram/domain/service/MuteService.java
@@ -0,0 +1,45 @@
+package fakegram.domain.service;
+
+import fakegram.adapter.cassandra.model.relation.RelationBySubject;
+import fakegram.domain.model.account.User;
+import fakegram.domain.repository.RelationRepository;
+
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static fakegram.domain.model.relation.RelationType.MUTE;
+
+public class MuteService {
+
+    private final UserService userService;
+    private final RelationRepository relationRepository;
+
+    @Inject
+    public MuteService(
+        final UserService userService,
+        final RelationRepository relationRepository
+    ) {
+        this.userService = userService;
+        this.relationRepository = relationRepository;
+    }
+
+    public Collection<User> getMutedUsers(UUID accountId) {
+        List<UUID> mutedUsersIds = relationRepository
+                .findAllRelationsBySubject(accountId, MUTE)
+                .stream()
+                .map(RelationBySubject::getObjectId)
+                .collect(Collectors.toList());
+        return userService.findUsersByAccountIds(mutedUsersIds);
+    }
+
+    public void muteUser(UUID accountId, UUID muteUserId) {
+        relationRepository.upsertRelation(accountId, muteUserId, MUTE);
+    }
+
+    public void unmuteUser(UUID accountId, UUID muteUserId) {
+        relationRepository.deleteRelation(accountId, muteUserId, MUTE);
+    }
+}
